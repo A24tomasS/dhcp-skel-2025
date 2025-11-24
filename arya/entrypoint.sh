0931b86fa91f79c7a589fb5017f61191fedd7139
Tom <a24tomass@iessanclemente.net>
2025-11-24 11:15:22 +0100

    2025-11-24

--- arya/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rsyslogd
-dhclient -v eth0
-named -f -u bind "$@"
-
---
+++ kea/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ip route del default via 192.168.10.1
+ip route add default via 192.168.10.254
+
+rm -f /var/run/kea/kea-dhcp4.kea-dhcp4.pid
+rm -f /var/run/kea/kea-dhcp-ddns.kea-dhcp-ddns.pid
+
+kea-dhcp-ddns -c /etc/kea/ddns-dhcp.conf -d &
+kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d &
+
+sleep infinity
